4371922a94c0f1e7392aa84710c80e52fe386c1d
Joe Topjian <joe@topjian.net>
2017-08-28 17:07:05 +0000

    updates

---
+++ local.sh
@@ -0,0 +1,29 @@
+sudo yum install -y git vim wget
+
+cd
+git clone https://github.com/jtopjian/dotfiles .dotfiles
+pushd .dotfiles
+bash create.sh
+popd
+
+source ~/.bashrc
+install_go
+source ~/.bashrc
+
+go get -u github.com/terraform-providers/terraform-provider-openstack
+pushd go/src/github.com/terraform-providers/terraform-provider-openstack
+git remote add jtopjian https://github.com/jtopjian/terraform-provider-openstack
+git fetch jtopjian
+popd
+
+go get -u github.com/hashicorp/terraform
+pushd go/src/github.com/hashicorp/terraform
+git remote add jtopjian https://github.com/jtopjian/terraform
+git fetch jtopjian
+popd
+
+go get -u github.com/gophercloud/gophercloud
+pushd go/src/github.com/gophercloud/gophercloud
+git remote add jtopjian https://github.com/jtopjian/gophercloud
+git fetch jtopjian
+popd
--- packstack-standard/files/deploy.sh
+++ packstack-standard/files/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -x
 
+sudo hostnamectl set-hostname localhost
+sudo yum -y update
+
 cd
 sudo yum install -y -q nfs-utils
 sudo mkdir /mnt/nfs
@@ -16,26 +19,6 @@ sudo yum install -y -q openstack-packstack crudini
 # Run packstack
 sudo packstack --answer-file /home/centos/files/packstack-answers.txt
 
-# Configure LBaaSv2 and FWaaS
-#sudo crudini --set /etc/neutron/neutron.conf DEFAULT debug True
-#sudo crudini --set /etc/neutron/l3_agent.ini DEFAULT debug True
-#sudo crudini --set /etc/neutron/neutron.conf DEFAULT service_plugins router,firewall,neutron_lbaas.services.loadbalancer.plugin.LoadBalancerPluginv2
-#sudo crudini --set /etc/neutron/neutron_lbaas.conf service_providers service_provider LOADBALANCERV2:Haproxy:neutron_lbaas.drivers.haproxy.plugin_driver.HaproxyOnHostPluginDriver:default
-#sudo crudini --set /etc/neutron/neutron.conf service_providers service_provider FIREWALL:Iptables:neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver:default
-#sudo crudini --set /etc/neutron/lbaas_agent.ini DEFAULT interface_driver neutron.agent.linux.interface.OVSInterfaceDriver
-#sudo crudini --set /etc/neutron/l3_agent.ini AGENT extensions fwaas
-#sudo crudini --set /etc/neutron/neutron.conf fwaas enabled True
-#sudo crudini --set /etc/neutron/neutron.conf fwaas driver iptables
-#sudo crudini --set /etc/neutron/neutron.conf fwaas agent_version v1
-
-#sudo neutron-db-manage --subproject neutron-lbaas upgrade head
-#sudo neutron-db-manage --subproject neutron-fwaas upgrade head
-#sudo systemctl disable neutron-lbaas-agent.service
-#sudo systemctl restart neutron-server.service
-#sudo systemctl restart neutron-l3-agent.service
-#sudo systemctl enable neutron-lbaasv2-agent.service
-#sudo systemctl start neutron-lbaasv2-agent.service
-
 # Move findmnt to allow multiple mounts to 127.0.0.1:/mnt
 sudo mv /bin/findmnt{,.orig}
 
@@ -50,6 +33,8 @@ _NETWORK_ID=$(openstack network show private -c id -f value)
 _SUBNET_ID=$(openstack subnet show private_subnet -c id -f value)
 _EXTGW_ID=$(openstack network show public -c id -f value)
 _IMAGE_ID=$(openstack image show cirros -c id -f value)
+
+echo "" >> /home/centos/keystonerc_admin
 echo export OS_IMAGE_NAME="cirros" >> /home/centos/keystonerc_admin
 echo export OS_IMAGE_ID="$_IMAGE_ID" >> /home/centos/keystonerc_admin
 echo export OS_NETWORK_ID=$_NETWORK_ID >> /home/centos/keystonerc_admin
@@ -58,6 +43,7 @@ echo export OS_POOL_NAME="public" >> /home/centos/keystonerc_admin
 echo export OS_FLAVOR_ID=99 >> /home/centos/keystonerc_admin
 echo export OS_FLAVOR_ID_RESIZE=98 >> /home/centos/keystonerc_admin
 
+echo "" >> /home/centos/keystonerc_demo
 echo export OS_IMAGE_NAME="cirros" >> /home/centos/keystonerc_demo
 echo export OS_IMAGE_ID="$_IMAGE_ID" >> /home/centos/keystonerc_demo
 echo export OS_NETWORK_ID=$_NETWORK_ID >> /home/centos/keystonerc_demo
@@ -65,6 +51,80 @@ echo export OS_EXTGW_ID=$_EXTGW_ID >> /home/centos/keystonerc_demo
 echo export OS_POOL_NAME="public" >> /home/centos/keystonerc_demo
 echo export OS_FLAVOR_ID=99 >> /home/centos/keystonerc_demo
 echo export OS_FLAVOR_ID_RESIZE=98 >> /home/centos/keystonerc_demo
+
+# Configure Swift
+sudo crudini --set /etc/swift/proxy-server.conf DEFAULT bind_ip 0.0.0.0
+
+# Install PowerDNS
+sudo mysql -e "CREATE DATABASE pdns default character set utf8 default collate utf8_general_ci"
+sudo mysql -e "GRANT ALL PRIVILEGES ON pdns.* TO 'pdns'@'localhost' IDENTIFIED BY 'password'"
+sudo yum install -y epel-release yum-plugin-priorities
+sudo curl -o /etc/yum.repos.d/powerdns-auth-40.repo https://repo.powerdns.com/repo-files/centos-auth-40.repo
+sudo yum install -y pdns pdns-backend-mysql
+
+echo "daemon=no
+allow-recursion=127.0.0.1
+config-dir=/etc/powerdns
+daemon=yes
+disable-axfr=no
+guardian=yes
+local-address=0.0.0.0
+local-ipv6=::
+local-port=53
+setgid=pdns
+setuid=pdns
+slave=yes
+socket-dir=/var/run
+version-string=powerdns
+out-of-zone-additional-processing=no
+webserver=yes
+api=yes
+api-key=someapikey
+launch=gmysql
+gmysql-host=127.0.0.1
+gmysql-user=pdns
+gmysql-dbname=pdns
+gmysql-password=password" | sudo tee /etc/pdns/pdns.conf
+
+sudo mysql pdns < /home/centos/files/pdns.sql
+sudo systemctl restart pdns
+
+# Install Designate
+openstack user create --domain default --password password designate
+openstack role add --project services --user designate admin
+openstack service create --name designate --description "DNS" dns
+openstack endpoint create --region RegionOne dns public http://127.0.0.1:9001/
+sudo mysql -e "CREATE DATABASE designate CHARACTER SET utf8 COLLATE utf8_general_ci"
+sudo mysql -e "CREATE DATABASE designate_pool_manager"
+sudo mysql -e "GRANT ALL PRIVILEGES ON designate.* TO 'designate'@'localhost' IDENTIFIED BY 'password'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON designate_pool_manager.* TO 'designate'@'localhost' IDENTIFIED BY 'password'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON designate.* TO 'designate'@'localhost' IDENTIFIED BY 'password'"
+
+sudo yum install -y openstack-designate\*
+designate_conf="/etc/designate/designate.conf"
+sudo cp /home/centos/files/pools.yaml /etc/designate/
+sudo crudini --set $designate_conf DEFAULT debug True
+sudo crudini --set $designate_conf DEFAULT debug True
+sudo crudini --set $designate_conf DEFAULT notification_driver messaging
+sudo crudini --set $designate_conf service:api enabled_extensions_v2 "quotas, reports"
+sudo crudini --set $designate_conf keystone_authtoken auth_uri http://127.0.0.1:5000
+sudo crudini --set $designate_conf keystone_authtoken auth_url http://127.0.0.1:35357
+sudo crudini --set $designate_conf keystone_authtoken username designate
+sudo crudini --set $designate_conf keystone_authtoken password password
+sudo crudini --set $designate_conf keystone_authtoken project_name services
+sudo crudini --set $designate_conf keystone_authtoken auth_type password
+sudo crudini --set $designate_conf service:worker enabled true
+sudo crudini --set $designate_conf service:worker notify true
+sudo crudini --set $designate_conf storage:sqlalchemy connection mysql+pymysql://designate:password@127.0.0.1/designate
+sudo -u designate designate-manage database sync
+
+sudo systemctl enable designate-central designate-api
+sudo systemctl enable designate-worker designate-producer designate-mdns
+sudo systemctl restart designate-central designate-api
+sudo systemctl restart designate-worker designate-producer designate-mdns
+
+sudo -u designate designate-manage pool update
+
 
 # Configure Manila
 manila type-create default_share_type True
@@ -74,28 +134,12 @@ sudo chown -R manila:manila /var/lib/manila/.ssh
 sudo crudini --set /etc/manila/manila.conf DEFAULT default_share_type default_share_type
 sudo crudini --set /etc/manila/manila.conf generic service_instance_user manila
 sudo crudini --set /etc/manila/manila.conf generic service_instance_password manila
-_MANILA_NET=$(openstack network show manila_service_network -c id -f value)
+source ~/keystone_demorc
+_SUBNET_ID=$(openstack subnet show private_subnet -c id -f value)
+manila share-network-create --neutron-net-id $_NETWORK_ID --neutron-subnet-id $_SUBNET_ID --name manila-network
+_MANILA_NET=$(manila share-network-list --columns id | grep [0-9] | awk '{print $2}')
 echo export OS_SHARE_NETWORK_ID=$_MANILA_NET >> /home/centos/keystonerc_admin
 echo export OS_SHARE_NETWORK_ID=$_MANILA_NET >> /home/centos/keystonerc_demo
-
-sudo yum install -y -q wget git vim
-sudo wget -O /usr/local/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
-sudo chmod +x /usr/local/bin/gimme
-/usr/local/bin/gimme 1.8 >> /home/centos/.bashrc
-
-mkdir ~/go
-eval "$(/usr/local/bin/gimme 1.8)"
-echo 'export GOPATH=$HOME/go' >> /home/centos/.bashrc
-export GOPATH=$HOME/go
-
-export PATH=$PATH:$HOME/terraform:$HOME/go/bin
-echo 'export PATH=$PATH:$HOME/terraform:$HOME/go/bin' >> .bashrc
-source .bashrc
-
-go get github.com/hashicorp/terraform
-go get github.com/gophercloud/gophercloud
-go get golang.org/x/crypto/...
-go get -u github.com/kardianos/govendor
 
 # Clean up the currently running services
 sudo systemctl stop openstack-cinder-backup.service
@@ -116,15 +160,18 @@ sudo systemctl stop neutron-metering-agent.service
 sudo systemctl stop openstack-manila-api.service
 sudo systemctl stop openstack-manila-scheduler.service
 sudo systemctl stop openstack-manila-share.service
-sudo systemctl stop httpd
+sudo systemctl stop designate-central designate-api
+sudo systemctl stop designate-worker designate-producer designate-mdns
 
 sudo mysql -e "update services set deleted_at=now(), deleted=id" cinder
 sudo mysql -e "update services set deleted_at=now(), deleted=id" nova
+sudo mysql -e "update compute_nodes set deleted_at=now(), deleted=id" nova
 for i in $(openstack network agent list -c ID -f value); do
   neutron agent-delete $i
 done
 
+sudo systemctl stop httpd
+
 # Copy rc.local for post-boot configuration
 sudo cp /home/centos/files/rc.local /etc
 sudo chmod +x /etc/rc.local
-
