f8ea9b1dc86fceee71aa87476b46aa86dc333c93
Joe Topjian <joe@topjian.net>
2020-06-24 16:57:14 +0000

    updates

--- devstack-standard/local.sh
+++ devstack-standard/local.sh
@@ -1,35 +1,31 @@
+sudo wget -O /usr/local/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
+sudo chmod +x /usr/local/bin/gimme
+gimme 1.12 >> .bashrc
+
+mkdir ~/go
+eval "$(/usr/local/bin/gimme 1.12)"
+echo 'export GOPATH=$HOME/go' >> .bashrc
+export GOPATH=$HOME/go
+
+export PATH=$PATH:$HOME/terraform:$HOME/go/bin
+echo 'export GO111MODULE=on' >> .bashrc
+echo 'export PATH=$PATH:$HOME/terraform:$HOME/go/bin' >> .bashrc
+source .bashrc
+
 cd
 git clone https://github.com/jtopjian/dotfiles .dotfiles
 pushd .dotfiles
 bash create.sh
 popd
 
-pushd go/src/github.com/hashicorp/terraform
-git remote add jtopjian https://github.com/jtopjian/terraform
+git clone https://github.com/terraform-providers/terraform-provider-openstack
+pushd terraform-provider-openstack
+git remote add jtopjian https://github.com/jtopjian/terraform-provider-openstack
 git fetch jtopjian
 popd
 
-source ~/.bashrc
-go get -u github.com/gophercloud/gophercloud
-pushd go/src/github.com/gophercloud/gophercloud
+git clone https://github.com/gophercloud/gophercloud
+pushd gophercloud
 git remote add jtopjian https://github.com/jtopjian/gophercloud
 git fetch jtopjian
 popd
-
-cat >> .bashrc.local <<EOF
-testacc() {
-  if [[ -n \$1 ]]; then
-    pushd ~/go/src/github.com/hashicorp/terraform
-    TF_LOG=DEBUG make testacc TEST=./builtin/providers/openstack TESTARGS="-run=\$1" 2>&1 | tee ~/openstack.log
-    popd
-  fi
-}
-
-gophercloudtest() {
-  if [[ -n \$1 ]] && [[ -n \$2 ]]; then
-    pushd  ~/go/src/github.com/gophercloud/gophercloud
-    go test -v -tags "fixtures acceptance" -run "\$1" github.com/gophercloud/gophercloud/acceptance/openstack/\$2 | tee ~/gophercloud.log
-    popd
-  fi
-}
-EOF
--- devstack-standard/main.tf
+++ devstack-standard/main.tf
@@ -1,52 +1,41 @@
 resource "openstack_compute_keypair_v2" "devstack-standard" {
-  name = "devstack-standard"
+  name       = "devstack-standard"
   public_key = "${file("key/id_rsa.pub")}"
 }
 
-resource "openstack_blockstorage_volume_v1" "devstack-standard" {
-  name = "devstack-standard"
-  size = 100
-}
-
 resource "openstack_compute_instance_v2" "devstack-standard" {
-  name = "devstack-standard"
-  image_name = "Ubuntu 14.04"
+  name        = "devstack-standard"
+  image_name  = "Ubuntu 16.04"
   flavor_name = "m1.xlarge"
 
-  key_pair = "${openstack_compute_keypair_v2.devstack-standard.name}"
+  key_pair        = "${openstack_compute_keypair_v2.devstack-standard.name}"
   security_groups = ["default", "AllowAll"]
 
-  volume {
-    volume_id = "${openstack_blockstorage_volume_v1.devstack-standard.id}"
+  network {
+    name = "default"
   }
 }
 
 resource "null_resource" "devstack-standard" {
   connection {
-    user = "ubuntu"
+    user        = "ubuntu"
     private_key = "${file("key/id_rsa")}"
-    host = "${openstack_compute_instance_v2.devstack-standard.access_ip_v6}"
+    host        = "${openstack_compute_instance_v2.devstack-standard.access_ip_v6}"
   }
 
   provisioner file {
-    source = "deploy.sh"
+    source      = "deploy.sh"
     destination = "/home/ubuntu/deploy.sh"
   }
 
   provisioner file {
-    source = "volume.sh"
-    destination = "/home/ubuntu/volume.sh"
-  }
-
-  provisioner file {
-    source = "local.sh"
+    source      = "local.sh"
     destination = "/home/ubuntu/local.sh"
   }
 
   provisioner "remote-exec" {
     inline = [
       "bash /home/ubuntu/deploy.sh",
-      "sudo bash /home/ubuntu/volume.sh",
       "bash /home/ubuntu/local.sh",
     ]
   }
